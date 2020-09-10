40c94266222c142283f7fe672aa0a4179e352b37
Muflah Nasir <muflahnasir@gmail.com>
2020-09-10 18:15:42 +0500

    Updated code

--- src/components/Editor/index.js
+++ src/components/Editor/index.js
@@ -135,9 +135,6 @@ function Editor({ id }) {
           command: "save-db",
           attributes: { title: "Save DB" },
         },
-      ]);
-
-      e.Panels.addButton("options", [
         {
           id: "fetch-code",
           className: "fa fa-get-pocket",
@@ -180,38 +177,6 @@ function Editor({ id }) {
             }); */
         },
       });
-      var blockManager = e.BlockManager;
-      blockManager.add("my-first-block", {
-        label: "Modal",
-        category: "Advanced",
-        attributes: { class: "fa fa-floppy-o" },
-        content: `<!-- Button trigger modal -->
-          <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#exampleModal">
-            Launch demo modal
-          </button>
-          
-          <!-- Modal -->
-          <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
-            <div class="modal-dialog" role="document">
-              <div class="modal-content">
-                <div class="modal-header">
-                  <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
-                  <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                    <span aria-hidden="true">&times;</span>
-                  </button>
-                </div>
-                <div class="modal-body">
-                  ...
-                </div>
-                <div class="modal-footer">
-                  <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-                  <button type="button" class="btn btn-primary">Save changes</button>
-                </div>
-              </div>
-            </div>
-          </div>`,
-      });
-
       // Remaining code
     } else {
       if (document) {
