13ece5f995fa99b9158dda3dff7bc2d2a32db1e7
Muflah Nasir <muflahnasir@gmail.com>
2020-09-11 16:27:15 +0500

    Update tab css

--- src/App.js
+++ src/App.js
@@ -7,11 +7,15 @@ import Editor from "./components/Editor";
 
 function App() {
   return (
-    <Tabs defaultActiveKey="landing" id="uncontrolled-tab-example">
+    <Tabs
+      defaultActiveKey="landing"
+      id="uncontrolled-tab-example"
+      className="myclass"
+    >
       <Tab eventKey="landing" title="Landing Page">
         <Editor id="editor" />
       </Tab>
-      <Tab eventKey="thankyou" title="Thankyou Page">
+      <Tab eventKey="thankyou" title="ThankYou Page">
         <Editor id="profile" />
       </Tab>
     </Tabs>
--- src/components/Editor/index.js
+++ src/components/Editor/index.js
@@ -27,7 +27,6 @@ let code = {
 };
 
 function Editor({ id }) {
-  debugger;
   let editor = null;
 
   useEffect(() => {
